81ea9b54c1fc9685119677600cb10b7e731eb0f6
Bhargav <141304156+bala-bhargav@users.noreply.github.com>
2026-01-17 17:38:49 +0530

    printing the error message

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    println!("invalid_grape_command");
+
+    
 }
