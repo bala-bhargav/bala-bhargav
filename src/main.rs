543295bde4d408512ca0e9452e8f05de8dd9ca55
Bhargav <141304156+bala-bhargav@users.noreply.github.com>
2026-01-18 08:06:31 +0530

    implementing the REPL

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,15 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
+    while(true){
     print!("$ ");
     io::stdout().flush().unwrap();
     let mut s = String::new();
-    io::stdin().read_line(&mut s).unwrap();
+    let bytes = io::stdin().read_line(&mut s).unwrap();
+    if(bytes == 0)break;
+    if( s.trim().is_empty())continue;
     println!("{}: command not found",s.trim());
+    }
 
 
     
