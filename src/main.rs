e7ff85a4a8ce6bd671fb8e07b71fc36465131329
Bhargav <141304156+bala-bhargav@users.noreply.github.com>
2026-01-22 23:16:40 +0530

    implement back slash

--- src/main.rs
+++ src/main.rs
@@ -111,17 +111,17 @@ fn main() {
      let mut chars = out.chars().peekable();
      let mut curr = String::new();
 
-  while let Some(c) = chars.next() {
-    if c == '\\' {
-        if let Some(next_char) = chars.next() {
-            curr.push(next_char);
+     while let Some(c) = chars.next(){
+        if c == '\''{
+            let Some(nxt_char) = chars.peek();
+            curr.push(nxt_char);
+            chars.next();
         }
-    } else {
-        curr.push(c);
-    }
-}
-
-println!("{}", curr);
+        else{
+            curr.push(c);
+        }
+     }
+     println!("{}",curr);
     }
     else if s.split_whitespace().nth(0) == Some("cat"){
         let args = parse_args(s.trim());
